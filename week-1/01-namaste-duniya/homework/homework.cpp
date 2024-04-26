99a0634139835a4471d0680b396317cd5fc9e9b3
Parit Bhardwaj <paritbhardwaj@outlook.com>
2024-04-26 22:44:58 +0530

    completed: lecture 1

---
+++ week-1/01-namaste-duniya/homework/homework.cpp
@@ -0,0 +1,10 @@
+#include <iostream> // import content of iostream header file, contain declarations of cin and cout
+using namespace std; // by this line you tell compiler to use std symbols or components defined in c++ instead of std::cout
+
+int main() 
+{          
+
+    cout << "Namaste Duniya :) \n"; // we can use \n for new line instead of endl;
+
+    return -1; // all behave same either it would be 0,1,12,-1,-2. using 0 is a good way to represent that this code is successfully executed!
+}
---
+++ week-1/01-namaste-duniya/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream> // header file include namespace
+using namespace std; // contains definition for cout and endl
+
+int main() // Starting point of the code
+{          // Scope of function main from { to }
+
+    cout << "Namaste Duniya :)" << endl;
+
+    return 0; // Successfull execution of code non zero values represent unsuccesfull code
+}
