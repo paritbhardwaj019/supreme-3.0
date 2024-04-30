da2fe672206cc114ade886c336a5c7feda5024d0
Parit Bhardwaj <paritbhardwaj@outlook.com>
2024-04-30 22:39:10 +0530

    completed: lecture 9

---
+++ week-1/09-operators/homework/homework.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // float a = 5.0; // % can be only use with int datatype
+
+    // cout << (a % 2) << endl;
+
+    return 0;
+}
---
+++ week-1/09-operators/main.cpp
@@ -0,0 +1,91 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // Unary operator
+    // int a = 5;
+
+    // // PRE INCREMENT
+    // cout << ++a << endl;
+
+    // // POST INCREMENT
+    // cout << a++ << endl;
+
+    // Arithmetic operator
+    // int a = 10;
+    // int b = 5;
+
+    // cout << a + b << endl;
+    // cout << a - b << endl;
+    // cout << a * b << endl;
+    // cout << a / b << endl;
+    // cout << a % b << endl;
+
+    // Relational operator
+    // int a = 10;
+    // int b = 5;
+
+    // cout << (a > b) << endl;
+    // cout << (a < b) << endl;
+    // cout << (a != b) << endl;
+    // cout << (a <= b) << endl;
+    // cout << (a >= b) << endl;
+    // cout << (a == b) << endl;
+
+    // Logical operator
+    // int a = 0;
+    // int b = 1;
+
+    // cout << (a && b) << endl;
+    // cout << (a || b) << endl;
+    // cout << (!a) << endl;
+
+    // Assignment operator
+    // int a = 0;
+
+    // a = 1;  // 1
+    // a += 1; // 2
+    // a -= 1; // 1
+    // a *= 2; // 2
+    // a /= 2; // 1
+    // a %= 1; // 0
+
+    // cout << a << endl;
+
+    // Bitwise operator
+    // cout << (5 & 6) << endl;
+    /*
+     1 - 1 = 1
+     0 - 1 = 0
+     1 - 0 = 0
+     0 - 0 = 0
+    */
+
+    // cout << (5 | 4) << endl; // 101 + 100 = 101 = 5
+    /*
+        1 - 1 = 1
+        0 - 1 = 1
+        1 - 0 = 1
+        0 - 0 = 0
+    */
+
+    // ~ = Tilde operator = flip bits
+    // cout << ~1 << endl;
+
+    // << = left shift operator
+    // cout << (5 << 2) << endl;
+
+    // >> = right shift operator
+    // cout << (10 >> 1) << endl;
+
+    // ^ = XOR operator
+    /*
+        0 - 1 = 0
+        1 - 0 = 0
+        1 - 1 = 1
+        0 - 0 = 1
+    */
+
+    return 0;
+}
