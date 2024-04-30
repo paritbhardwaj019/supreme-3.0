af9bc986004f603c6796f178b32051d3bb6a104e
Parit Bhardwaj <paritbhardwaj@outlook.com>
2024-04-30 09:54:10 +0530

    completed: lecture 6

---
+++ week-1/06-ternary-operator/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x = 10;
+    int y = 20;
+
+    int result = (x > y) ? x : y;
+
+    cout << "RESULT: " << result << endl;
+
+    return 0;
+}
